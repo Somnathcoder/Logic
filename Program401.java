47cb98a45627856779fcf72a54dfa9e5fc07f811
Somnathcoder <124282824+Somnathcoder@users.noreply.github.com>
2023-02-03 22:45:44 +0530

    Add files via upload

---
+++ Program401.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Program401
+{
+    public static void main(String Ar[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter file name : ");
+        String FileName = sobj.nextLine();
+
+        System.out.println("File name is : "+FileName);
+    }
+}
